54debc590a61e84a6df7775d1e2eef4736a81356
Harin Kim <62981406+rineeee@users.noreply.github.com>
2023-08-22 21:25:08 +0900

    Delete catalog-service directory

--- catalog-service/src/main/java/com/example/catalogservice/repository/CatalogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.catalogservice.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CatalogRepository extends CrudRepository<CatalogEntity, Long> {
-    CatalogEntity findByProductId(String productId);
-}
